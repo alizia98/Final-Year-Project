2591b57c314a558f8f354c81f35077b2c79a421e
alizia98 <31632250+alizia98@users.noreply.github.com>
2020-04-08 15:30:42 +0500

    app bar added

--- src/components/header.js
+++ src/components/header.js
@@ -3,53 +3,46 @@ import { withRouter } from "react-router";
 import { Navbar, Container, Row } from "react-bootstrap";
 import { useAuth0 } from "../auth/react-auth0-wrapper";
 import { Link } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }));
+  
 
 function Header() {
+  const classes = useStyles();
   const { isAuthenticated, loginWithRedirect, logout, user } = useAuth0();
-  return (
-    <div className="header">
-      <Container className="navbar">
-        <Navbar>
-          <Row>
-            <Link className="fw anchor" to="/">
-              &nbsp; Hacker News
-            </Link>
-          </Row>
-          <Row className="ml-auto">
-            {!isAuthenticated && (
-              <>
-                <button
-                  className="auth-button"
-                  onClick={() => loginWithRedirect({})}
-                >
-                  Log in
-                </button>
-              </>
-            )}
 
-            {isAuthenticated && user && (
-              <>
-                <Link className="anchor" to="/new-post">
-                  submit
-                </Link>
-                <span> &nbsp;|&nbsp; </span>
-                <span className="anchor">
-                  user :
-                  <Link className="anchor" to={"/user/" + user.sub}>
-                    {user.nickname}
-                  </Link>
-                </span>
-                <span> &nbsp;|&nbsp; </span>
-                <button className="auth-button" onClick={() => logout()}>
-                  Log out
-                </button>
-              </>
-            )}
-          </Row>
-        </Navbar>
-      </Container>
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+        
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Step by Step Web Portal
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
     </div>
   );
+
 }
 
 export default withRouter(Header);
